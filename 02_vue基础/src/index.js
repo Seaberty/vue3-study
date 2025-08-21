c1af7fbc94dae56fec5852f6e4dd007c83164969
Seabert <351338629@qq.com>
2025-08-21 10:09:38 +0800

    更该项目目录名称

---
+++ 02_vue基础/src/index.js
@@ -0,0 +1,17 @@
+// 引入 vue
+// 这里引入的 vue 版本，默认不支持通过 template 属性来设置模版
+import { createApp } from "vue/dist/vue.esm-bundler.js";
+
+// 创建一个根组件
+const Root = {
+    data() {
+        return {
+            message: "I love Vue"
+        }
+    },
+    template: "<h1>{{message}}</h1>"
+}
+
+// 挂载实例
+// mount() 的返回值就是根组件的实例
+const vm = createApp(Root).mount('#app')
--- 02_vue的基础/02_hello_vue/src/index.js
+++
@@ -1,8 +0,0 @@
-// 入口文件
-import { createApp } from 'vue/dist/vue.esm-bundler.js'
-import App from './App.vue'
-
-
-// 挂载实例
-// mount() 的返回值就是根组件的实例
-const vm = createApp(App).mount('#app')
